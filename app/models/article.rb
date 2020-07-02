c2b829cdb5762a8b835be071fa781f20c1f8fb38
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-02 16:28:47 -0500

    Add image association and validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ApplicationRecord
   has_many :votes, dependent: :destroy
   has_many :taggings
   has_many :categories, through: :taggings
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
 end
