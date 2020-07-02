15cd981ce5d3753a98ccfe67a1cb023da8c493b1
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-02 16:58:52 -0500

    Fix typos

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
-  belongs_to :author, classname: 'User'
+  belongs_to :author, class_name: 'User'
   has_many :votes, dependent: :destroy
   has_many :taggings
   has_many :categories, through: :taggings
-  has_attached_file :image
-  validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
+  has_one_attached :image
+  validates_acceptance_of :image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
 end
