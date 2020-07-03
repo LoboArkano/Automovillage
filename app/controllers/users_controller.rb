dbddd2fc66546853e94ab7842ecd2c5723f76039
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-03 16:40:28 -0500

    Add index post view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  include UsersHelper
+
   def new
     @user = User.new
   end
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to sessions_log_in_path
     end
   end
 
