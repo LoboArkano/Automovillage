54990144ab04a913a94d3972af17c3b809994a5d
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-03 16:39:30 -0500

    Add sessions views
    Add log in form
    Add register form
    Update routes
    Add current_user method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/log_in'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: %i[new create log_in destroy]
   resources :users, only: %i[new create show]
