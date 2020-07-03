c2bc922a0c13146d4b488e4796ffcffe9d738a60
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-03 12:32:59 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: %i[new create log_in destroy]
-  root "home#index"
+  resources :users, only: %i[new create show]
+  resources :articles, only: %i[new create index show]
+  root "articles#index"
 end
