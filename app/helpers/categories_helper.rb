f5f184082e2a1ec1c4280a6d093d930e871fe138
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-14 16:44:25 -0500

    Add style for most recent article by category

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -5,8 +5,28 @@ module CategoriesHelper
 
   def most_voted_article
     article = Article.all.order('votes_count IS NULL, votes_count desc').first
-    content_tag(:article, class: "mv-article d-flex w-100", style: "background-image: url(#{article.picture.service_url})") do
-      concat content_tag(:p, "#{article.title}", class: "mv-title") + content_tag(:p, "#{article.text}", class: "mv-text")
+    content_tag(:article, class: "mv-article bg-custom d-flex w-100", style: "background-image: url(#{article.picture.service_url})") do
+      concat(
+        content_tag(:p, "#{article.title}", class: "mv-title") + 
+        content_tag(:p, "#{article.text}", class: "mv-text")
+      )
+    end
+  end
+
+  def most_recent_art_by_category(categories)
+    content_tag(:div, class: "categories-container d-flex") do
+      categories.collect do |category|
+        break if category.priority > 4
+
+        article = category.articles.order(created_at: :desc).first
+        if article != nil
+          concat content_tag(:article,
+                              content_tag(:p, link_to("#{category.name}", categories_show_path(category), class: "category-name")) + 
+                              content_tag(:p, "#{article.title}", class: "article-title"),
+                              class: 'mr-article bg-custom d-flex',
+                              style: "background-image: url(#{article.picture.service_url})")
+        end
+      end
     end
   end
 end
