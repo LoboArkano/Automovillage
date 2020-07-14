cf50826f8f837bf1e6b7d365515f48dad818bb87
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-14 12:52:37 -0500

    Add style for most voted article
    Create most_voted_article method helper
    Update footer

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -2,4 +2,11 @@ module CategoriesHelper
   def categories_order_by_priority
     Category.all.order(:priority)
   end
+
+  def most_voted_article
+    article = Article.all.order('votes_count IS NULL, votes_count desc').first
+    content_tag(:article, class: "mv-article d-flex w-100", style: "background-image: url(#{article.picture.service_url})") do
+      concat content_tag(:p, "#{article.title}", class: "mv-title") + content_tag(:p, "#{article.text}", class: "mv-text")
+    end
+  end
 end
