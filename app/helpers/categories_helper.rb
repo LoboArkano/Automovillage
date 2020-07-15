8b312cce024fc0ff620c35223b99f566e4614222
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-15 11:40:40 -0500

    Add style to the show category view

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -20,12 +20,40 @@ module CategoriesHelper
 
         article = category.articles.order(created_at: :desc).first
         if article != nil
-          concat content_tag(:article,
-                              content_tag(:p, link_to("#{category.name}", categories_show_path(category), class: "category-name")) + 
-                              content_tag(:p, "#{article.title}", class: "article-title"),
-                              class: 'mr-article bg-custom d-flex',
-                              style: "background-image: url(#{article.picture.service_url})")
+          concat(
+            content_tag(:article,
+                        content_tag(:p, link_to("#{category.name}", categories_show_path(category), class: "category-name")) + 
+                        content_tag(:p, "#{article.title}", class: "article-title"),
+                        class: 'mr-article bg-custom d-flex',
+                        style: "background-image: url(#{article.picture.service_url})")
+          )
         end
+      end
+    end
+  end
+
+  def most_recent_articles(category, articles)
+    content_tag(:div, class: "articles-container d-flex") do
+      articles.collect do |article|
+        if current_user
+          if article.votes.where(:user_id => session[:user_id], :article_id => article.id).size.zero?
+            vote = content_tag(:p, link_to("#{article.votes.size} - Add Vote", votes_create_path(article), class: "vote orange"))
+          else
+            vote = content_tag(:p, link_to("#{article.votes.size} - Remove Vote", votes_destroy_path(article), class: "vote gray"))
+          end
+        end
+        concat(
+          content_tag(:article,
+                      content_tag(:div, cl_image_tag("#{article.picture.key}", class: "bg-custom-2 w-100 h-100"), class: "img-container") + 
+                      content_tag(:div,
+                                  content_tag(:p, "#{category.name}", class: "art-category-name") + 
+                                  content_tag(:p, "#{article.title}", class: "art-title") + 
+                                  content_tag(:p, "#{article.text}", class: "art-text") + 
+                                  content_tag(:p, "Author: #{article.author.name}") + 
+                                  vote,
+                                  class: "art-info"),
+                      class: "article-container light-gray-bg d-flex")
+        )
       end
     end
   end
