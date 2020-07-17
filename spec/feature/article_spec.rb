bb85ef2546399159e9a2582e0755cbfb9dc6d871
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-17 00:45:19 -0500

    Fix rubocop errors

--- spec/feature/article_spec.rb
+++ spec/feature/article_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe 'Webpage workflow', type: :system do
   describe 'Navigate the webpage' do
     it 'create an article and like it' do
       User.create(name: 'Jaskier')
-      Category.create(name: 'Formula 1',priority: 1)
+      Category.create(name: 'Formula 1', priority: 1)
       visit sessions_log_in_path
       fill_in 'Enter your username:', with: 'Jaskier'
       click_on 'Login'
       sleep(3)
       click_on 'CREATE ARTICLE'
       fill_in 'article[title]', with: 'NASCAR hosts largest sporting event crowd since pandemic'
-      fill_in 'article[text]', with: "BRISTOL, Tenn. (AP) — Clint Bowyer hopped off his pit stand at Bristol Motor Speedway to learn he’d won the fan vote to advance into NASCAR’s All-Star race — an announcement met by a roar from the grandstands."
+      fill_in 'article[text]', with: 'BRISTOL, Tenn. — Clint Bowyer hopped off his pit stand at Bristol Motor Speedway'
       find('form input[type="file"]').set('./spec/images/nascar.jpeg')
       check 'category_ids_'
       sleep(3)
